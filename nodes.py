b839e5042d11c2c123e1e682d5734f49d74d2395
aria1th <35677394+aria1th@users.noreply.github.com>
2023-12-13 00:45:57 +0900

    Math of course

--- nodes.py
+++ nodes.py
@@ -2,6 +2,7 @@
 from .logic_gates import CLASS_MAPPINGS as LogicMapping, CLASS_NAMES as LogicNames
 from .randomness import CLASS_MAPPINGS as RandomMapping, CLASS_NAMES as RandomNames
 from .conversion import CLASS_MAPPINGS as ConversionMapping, CLASS_NAMES as ConversionNames
+from .math_nodes import CLASS_MAPPINGS as MathMapping, CLASS_NAMES as MathNames
 from .exif.exif import read_info_from_image_stealth
 from .autonode import node_wrapper, get_node_names_mappings, validate
 import time
@@ -136,6 +137,7 @@ NODE_CLASS_MAPPINGS.update(classes_parsed)
 NODE_CLASS_MAPPINGS.update(LogicMapping)
 NODE_CLASS_MAPPINGS.update(RandomMapping)
 NODE_CLASS_MAPPINGS.update(ConversionMapping)
+NODE_CLASS_MAPPINGS.update(MathMapping)
 NODE_DISPLAY_NAME_MAPPINGS = {
 
 }
@@ -143,3 +145,4 @@ NODE_DISPLAY_NAME_MAPPINGS.update(node_names_parsed)
 NODE_DISPLAY_NAME_MAPPINGS.update(LogicNames)
 NODE_DISPLAY_NAME_MAPPINGS.update(RandomNames)
 NODE_DISPLAY_NAME_MAPPINGS.update(ConversionNames)
+NODE_DISPLAY_NAME_MAPPINGS.update(MathNames)
