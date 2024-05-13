9d5c3b66050f8f7f8d4569fe82289becf6b6d635
AngelBottomless <aria1th@naver.com>
2024-05-13 09:25:11 +0900

    Add some description for future usages

--- autonode.py
+++ autonode.py
@@ -1,4 +1,47 @@
+"""
+AutoNode setup - AngelBottomless@github
+# By following the example, you can prepare "decorator" that will automatically collect required information for node registration.
+fundamental_classes = []
+fundamental_node = node_wrapper(fundamental_classes)
 
+# Then, you can define the classes that will be used in the node. "FUNCTION", "INPUT_TYPES", "RETURN_TYPES", "CATEGORY" attributes are used for node registration.
+# You can set "custom_name" attribute to set the name of the node that will be displayed in the UI.
+# Pleare run validate(fundamental_classes) to check if all required attributes are set.
+# You can also use anytype to represent any type of input.
+@fundamental_node
+class SleepNodeAny:
+    FUNCTION = "sleep"
+    RETURN_TYPES = (anytype,)
+    CATEGORY = "Misc"
+    custom_name = "SleepNode"
+    @staticmethod
+    def sleep(interval, inputs):
+        time.sleep(interval)
+        return (inputs,)
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "interval": ("FLOAT", {"default": 0.0}),
+            },
+            "optional": {
+                "inputs": (anytype, {"default": 0.0}),
+            }
+
+# Then, at the end of each node registeration class, run the following to set up static variables.
+CLASS_MAPPINGS, CLASS_NAMES = get_node_names_mappings(fundamental_classes)
+
+# Then, at the other script - here, nodes.py, you can import the CLASS_MAPPINGS and CLASS_NAMES to register the nodes.
+from .io_node import CLASS_MAPPINGS as IOMapping, CLASS_NAMES as IONames
+
+# it collects NODE_CLASS_MAPPINGS and NODE_DISPLAY_NAME_MAPPINGS, and updates them with the new mappings. Note that same keys will be overwritten.
+
+# Finally, at the __init__.py, you can import the NODE_CLASS_MAPPINGS and NODE_DISPLAY_NAME_MAPPINGS to register the nodes.
+from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+
+# Then, you can use the registered nodes in the UI!
+"""
 def get_node_names_mappings(classes):
     node_names = {}
     node_classes = {}
@@ -21,7 +64,7 @@ def validate(container):
         for attr in ["FUNCTION", "INPUT_TYPES", "RETURN_TYPES", "CATEGORY"]:
             if not hasattr(cls, attr):
                 raise Exception("Class {} doesn't have attribute {}".format(cls.__name__, attr))
-            
+# AllTrue class hijacks the isinstance, issubclass, bool, str, jsonserializable, eq, ne methods to always return True
 class AllTrue(str):
     def __init__(self, representation=None) -> None:
         self.repr = representation
