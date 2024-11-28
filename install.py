131e554e9cdc53427c6f6e193a50c9e3c3e99853
AngelBottomless <aria1th@naver.com>
2024-11-29 02:20:29 +0900

    fix install

--- install.py
+++ install.py
@@ -1,23 +1,56 @@
-import launch
-import pkg_resources
+#https://github.com/ltdrdata/ComfyUI-Impact-Pack/blob/Main/install.py
+import sys
+import subprocess
+import threading
+import locale
 
-def get_installed_version(package: str):
-    try:
-        return pkg_resources.get_distribution(package).version
-    except Exception:
-        return None
+def handle_stream(stream, is_stdout):
+    stream.reconfigure(encoding=locale.getpreferredencoding(), errors='replace')
+
+    for msg in stream:
+        if is_stdout:
+            print(msg, end="", file=sys.stdout)
+        else: 
+            print(msg, end="", file=sys.stderr)
+
+def process_wrap(cmd_str, cwd=None, handler=None):
+    process = subprocess.Popen(cmd_str, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, bufsize=1)
+
+    if handler is None:
+        handler = handle_stream
+
+    stdout_thread = threading.Thread(target=handler, args=(process.stdout, True))
+    stderr_thread = threading.Thread(target=handler, args=(process.stderr, False))
+
+    stdout_thread.start()
+    stderr_thread.start()
+
+    stdout_thread.join()
+    stderr_thread.join()
+
+    return process.wait()
+
+if "python_embeded" in sys.executable or "python_embedded" in sys.executable: #standalone python version
+    pip_install = [sys.executable, '-s', '-m', 'pip', 'install', "-U"]
+else:
+    pip_install = [sys.executable, '-m', 'pip', 'install', "-U"]
 
 def initialization():
-    for pkg_name in ["chardet", "piexif"]:
-        run_installation(pkg_name)
+    try:
+        import piexif
+    except ImportError:
+        run_installation("piexif")
+    try:
+        import chadet
+    except ImportError:
+        run_installation("chardet")
 
 def run_installation(pkg_name: str):
-    is_not_installed = get_installed_version(pkg_name) is None
-    if not is_not_installed:
-        return
-    launch.run_pip(
-        f'install -U "{pkg_name}"',
-        f"Install {pkg_name}",
-    )
+    print(f"Installing {pkg_name}...")
+    if process_wrap(pip_install + [pkg_name]) == 0:
+        print(f"Successfully installed {pkg_name}")
+    else:
+        print(f"Failed to install {pkg_name}")
 
-initialization()
+if __name__ == "__main__":
+    initialization()
